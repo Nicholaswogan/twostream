5ab5099c3d0d816dd7a572b8d0352164fea79696
Nick Wogan <nicholaswogan@gmail.com>
2022-10-22 11:48:19 -0700

    reproduced table 6 in Toon et al

--- README.md
+++ README.md
@@ -1,20 +1,4 @@
 # twostream
 
-This repository contains the two-stream radiative transfer algorithm from [Toon et al. (1989)](https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/JD094iD13p16287) written in Python, C++, and Fortran.
-
-Each implementation has the following inputs:
-
-- `nz` - The number of vertical layers in the atmosphere.
-- `tau` - The optical depth of each layer in the atmosphere. The first element is the top layer and the last element is the surface layer.
-- `w0` - The single scattering albedo of each layer in the atmosphere.
-- `u0` - cosine of the solar zenith angle.
-- `Rsfc` - Albedo of the surface.
-
-and the following output:
-
-- `amean` - The mean intensity at the edges of each layer in the atmosphere. The first element is the top edge op the top layer
-
-Every term is unit-less, and the flux at the top of the atmosphere is taken to be 1.
-
-I use this algorithm to compute photolysis rates in photochemical modeling. [Here is a link to an example](https://github.com/Nicholaswogan/Photochem/blob/11fcdda980e13983d816e9f8e42a4a4ea123be12/src/photochem.f90#L364).
+This repository contains the two-stream radiative transfer algorithm from [Toon et al. (1989)](https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/JD094iD13p16287) written in Modern Fortran.
 
--- test/test_twostream.f90
+++ test/test_twostream.f90
@@ -5,16 +5,27 @@ program test_twostream
   use test_utils, only: TwostreamTestData
   implicit none
 
+  write(*,"(A)") ""
+  write(*,"(A)") "~~~~~~~~~~~~~~~~~~~~"
+  write(*,"(A)") "~~~ Memory Tests ~~~"
+  write(*,"(A)") "~~~~~~~~~~~~~~~~~~~~"
+  write(*,"(A)") ""
   call test_twostream_solar_noalloc()
   call test_twostream_solar_alloc()
-  call test_twostream_solar_table_6()
-
   call test_twostream_ir_noalloc()
   call test_twostream_ir_alloc()
 
+  write(*,"(A)") ""
+  write(*,"(A)") "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  write(*,"(A)") "~~~ Reproducing Toon et al. (1989) ~~~"
+  write(*,"(A)") "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  call test_twostream_solar_table_6()
+
 contains
 
-  !~~ two_stream_solar tests ~~!
+  !~~~~~~~~~~~~~~~~~~!
+  !~~ Memory Tests ~~!
+  !~~~~~~~~~~~~~~~~~~!
 
   subroutine test_twostream_solar_noalloc()
     type(TwostreamTestData) :: d
@@ -36,108 +47,6 @@ contains
     print*,'fup(1) = ',d%fup(1)
   end subroutine
 
-  subroutine test_twostream_solar_table_6()
-    type(TwostreamTestData) :: d
-
-    real(dp), parameter :: u0(*) = [0.1_dp, 0.1_dp, 0.1_dp, 0.4_dp, 0.4_dp, 0.4_dp, &
-                                    0.92_dp, 0.92_dp, 0.92_dp]
-    real(dp), parameter :: tau(*) = [0.02_dp, 0.25_dp, 1.0_dp, 0.02_dp, 0.25_dp, 1.0_dp, &
-                                     0.02_dp, 0.25_dp, 1.0_dp]
-    real(dp), parameter :: Rsfc(*) = [0.0_dp, 0.25_dp, 0.8_dp]
-
-    character(15) :: tmp_str
-    character(:), allocatable :: line
-    real(dp) :: amean1(size(tau),size(Rsfc)), amean2(size(tau),size(Rsfc))
-    integer :: i,j
-
-    call d%allocate(1)
-
-    do i = 1,size(Rsfc)
-      do j = 1,size(tau)
-        d%tau = tau(j)
-        d%w0 = 0.9999999_dp
-        d%gt = 0.0_dp
-        d%u0 = u0(j)
-        d%Rsfc = Rsfc(i)
-        call two_stream_solar(d%nz, d%tau, d%w0, d%gt, d%u0, d%Rsfc, d%amean, d%surface_radiance, d%fup, d%fdn)
-        amean1(j,i) = d%amean(1)
-        amean2(j,i) = d%amean(2)
-      enddo
-    enddo
-
-    if (allocated(line)) deallocate(line)
-    allocate(character(0)::line)
-    write(tmp_str,"(A)") "u0"
-    line = line//tmp_str
-    write(tmp_str,"(A)") "tau"
-    line = line//tmp_str
-    write(tmp_str,"(A)") "Rsfc = 0"
-    line = line//tmp_str
-    write(tmp_str,"(A)") "Rsfc = 0.25"
-    line = line//tmp_str
-    write(tmp_str,"(A)") "Rsfc = 0.80"
-    line = line//tmp_str
-    write(*,"(A)") line
-    do i = 1,len(line)
-      line(i:i) = '-'
-    enddo
-    write(*,"(A)") line
-    
-    do j = 1,size(tau)
-      if (allocated(line)) deallocate(line)
-      allocate(character(0)::line)
-
-      call round_format_string(u0(j), tmp_str)
-      line = line//tmp_str
-      
-      call round_format_string(tau(j), tmp_str)
-      line = line//tmp_str
-
-      do i = 1,size(Rsfc)
-        call round_format_string(amean1(j,i), tmp_str)
-        line = line//tmp_str
-      enddo
-
-      write(*,"(A)") line
-    enddo
-    write(*,"(A)") ""
-    do j = 1,size(tau)
-      if (allocated(line)) deallocate(line)
-      allocate(character(0)::line)
-
-      call round_format_string(u0(j), tmp_str)
-      line = line//tmp_str
-      
-      call round_format_string(tau(j), tmp_str)
-      line = line//tmp_str
-
-      do i = 1,size(Rsfc)
-        call round_format_string(amean2(j,i), tmp_str)
-        line = line//tmp_str
-      enddo
-
-      write(*,"(A)") line
-    enddo
-
-  end subroutine
-
-  subroutine round_format_string(a, b)
-    real(dp), intent(in) :: a
-    character(*), intent(out) :: b
-
-    integer :: tmp_i
-    real(dp) :: tmp_r
-
-    tmp_i = int(a*1000.0_dp)
-    tmp_r = tmp_i/1000.0_dp
-
-    write(b,"(f15.3)") tmp_r
-    b = adjustl(b)
-
-  end subroutine
-
-  !~~ two_stream_ir tests ~~!
-
   subroutine test_twostream_ir_noalloc()
     type(TwostreamTestData) :: d
     type(TwoStreamIRWrk) :: wrk
@@ -156,6 +65,128 @@ contains
     call two_stream_ir(d%nz, d%tau, d%w0, d%gt, 0.0_dp, d%bplanck, d%fup, d%fdn)
     print*,'test_twostream_ir_alloc:'
     print*,'fup(1) = ',d%fup(1)
+  end subroutine
+  
+  !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+  !~~ Reproducing Toon et al. (1989) ~~!
+  !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+
+  subroutine test_twostream_solar_table_6()
+    type(TwostreamTestData) :: d
+
+    real(dp), parameter :: u0(*) = [0.1_dp, 0.1_dp, 0.1_dp, 0.4_dp, 0.4_dp, 0.4_dp, &
+                                    0.92_dp, 0.92_dp, 0.92_dp]
+    real(dp), parameter :: tau(*) = [0.02_dp, 0.25_dp, 1.0_dp, 0.02_dp, 0.25_dp, 1.0_dp, &
+                                     0.02_dp, 0.25_dp, 1.0_dp]
+    real(dp), parameter :: Rsfc(*) = [0.0_dp, 0.25_dp, 0.8_dp]
+
+    character(15) :: tmp_str
+    character(:), allocatable :: line, labels, under_labels
+    real(dp) :: amean1(size(tau),size(Rsfc)), amean2(size(tau),size(Rsfc))
+    integer :: i,j
+
+    call d%allocate(1)
+
+    do i = 1,size(Rsfc)
+      do j = 1,size(tau)
+        d%tau = tau(j)
+        d%w0 = 0.9999999_dp
+        d%gt = 0.0_dp
+        d%u0 = u0(j)
+        d%Rsfc = Rsfc(i)
+        call two_stream_solar(d%nz, d%tau, d%w0, d%gt, d%u0, d%Rsfc, d%amean, d%surface_radiance, d%fup, d%fdn)
+        amean1(j,i) = d%amean(1)
+        amean2(j,i) = d%amean(2)
+      enddo
+    enddo
+
+    write(*,"(A)") ""
+    write(*,"(A)") "**Table 6: Mean Intensities for Conservative Rayleigh Scatter (J(0)/(\pi*F_s))**"
+
+    if (allocated(line)) deallocate(line)
+    allocate(character(0)::line)
+    tmp_str = "| u0"
+    line = line//tmp_str
+    tmp_str = "| tau"
+    line = line//tmp_str
+    tmp_str = "| Rsfc = 0"
+    line = line//tmp_str
+    tmp_str = "| Rsfc = 0.25"
+    line = line//tmp_str
+    tmp_str = "| Rsfc = 0.80"
+    line = line//tmp_str
+    line = line//"|"
+    labels = line
+    write(*,"(A)") line
+
+    if (allocated(line)) deallocate(line)
+    allocate(character(0)::line)
+    tmp_str = "| ------------ "
+    do i = 1,5
+      line = line//tmp_str
+    enddo
+    line = line//"|"
+    under_labels = line
+    write(*,"(A)") line
+    
+    do j = 1,size(tau)
+      if (allocated(line)) deallocate(line)
+      allocate(character(0)::line)
+
+      call round_format_string(u0(j), tmp_str)
+      line = line//tmp_str
+      
+      call round_format_string(tau(j), tmp_str)
+      line = line//tmp_str
+
+      do i = 1,size(Rsfc)
+        call round_format_string(amean1(j,i), tmp_str)
+        line = line//tmp_str
+      enddo
+      line = line//"|"
+      write(*,"(A)") line
+    enddo
+
+
+    write(*,"(A)") ""
+    write(*,"(A)") "**Table 6: Mean Intensities for Conservative Rayleigh Scatter (J(\tau)/(\pi*F_s))**"
+    write(*,"(A)") labels
+    write(*,"(A)") under_labels
+    do j = 1,size(tau)
+      if (allocated(line)) deallocate(line)
+      allocate(character(0)::line)
+
+      call round_format_string(u0(j), tmp_str)
+      line = line//tmp_str
+      
+      call round_format_string(tau(j), tmp_str)
+      line = line//tmp_str
+
+      do i = 1,size(Rsfc)
+        call round_format_string(amean2(j,i), tmp_str)
+        line = line//tmp_str
+      enddo
+      line = line//"|"
+      write(*,"(A)") line
+    enddo
+
+  end subroutine
+
+  subroutine round_format_string(a, b)
+    real(dp), intent(in) :: a
+    character(*), intent(out) :: b
+
+    integer :: tmp_i
+    real(dp) :: tmp_r
+
+    ! rounding
+    tmp_i = int(a*1000.0_dp)
+    tmp_r = tmp_i/1000.0_dp
+
+    ! formating
+    write(b,"(f15.3)") tmp_r
+    b = "| "//adjustl(b)
+
   end subroutine
 
 end program
